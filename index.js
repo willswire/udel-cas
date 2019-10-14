e3143a022743266a6b803bd6d286a01fdfe70faf
Will Walker <wwalker@udel.edu>
2019-10-14 10:38:06 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const querystring = require('querystring');
+var https = require('https');
+var fs = require('fs');
+
+var casLogin = '/cas/login?'
+var casServer = 'https://cas.nss.udel.edu'
+var serviceURL = 'https://planner.cis.udel.edu:3000/'
+
+var serviceQueryString = querystring.stringify({
+    service: serviceURL
+});
+
+app.get('/', function (req, res) {
+    res.redirect(302, casServer + casLogin + serviceQueryString);
+});
+
+https.createServer({
+        key: fs.readFileSync('/var/secret/etc/ssl/forms-combined.cis.udel.edu.key'),
+        cert: fs.readFileSync('/var/secret/etc/ssl/forms-combined.cis.udel.edu.pem')
+    }, app)
+    .listen(3000, function () {
+        console.log('App listening on port 3000! Go to: ' + serviceURL)
+    });
