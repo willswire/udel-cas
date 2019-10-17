b67da7f4302430f95088e21560b4b5ff96db2031
Will Walker <wwalker@udel.edu>
2019-10-17 11:19:15 -0400

    just one

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/', async (req, res) => {
                 compact: true,
                 trim: true
             });
-            var casCookie = jwt.sign(jsonData["cas:serviceResponse"]["cas:authenticationSuccess"], 'planner');
+            var casCookie = jwt.sign(jsonData[1], 'planner');
             res.cookie(
                 'cas_user',
                 casCookie, {
