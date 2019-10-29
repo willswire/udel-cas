f6df81c9f12f11768730f1fe8a29d74fd19dd0a4
MadeByAydin <muhammet@udel.edu>
2019-10-29 01:58:47 -0400

    new api endpoint to support better (maybe?) way of displaying semesters

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var mongo = require('mongoose');
 var Plan = require('./model/Plan.js');
 var User =require('./model/User.js');
+var serviceURL = 'https://planner.cis.udel.edu:3002';
 const port = 3002;
 
 /*
@@ -28,7 +29,7 @@ var db = mongo.connect("mongodb+srv://muhammet:test123@cluster0-dg6n3.mongodb.ne
  app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -38,61 +39,120 @@ app.use(function(req, res, next) {
 app.post('/api/update-plan/:planID', function(req, res){
     Plan.save({"planID":req.params.planID}, function(err, data){
         if(err){
-            res.send(err);
+            res.status(400).send(err);
         }
-
-        res.send({data: "The plan was deleted"});
+        else{
+            res.status(200).send({data: "The plan was deleted"});
+        }
     });
 });
 
 app.get('/api/plans/:planID', function(req, res){
     Plan.find({"planID":req.params.planID}, function(err, data){
         if(err){
-            res.send(err);
+            res.status(400).send(err);
         }
 
-        console.log(data);
-        res.send(data);
+        else{
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.get('/api/plans/semesterize/:planID', function(req, res){
+    Plan.find({"planID":req.params.planID}, function(err, data){
+        if(err){
+            res.status(400).send(err);
+        }
+
+        else{
+            let curriculum = data[0];
+            semesters = [];
+
+            if(curriculum){
+                for(var i = 1; i <= 8; i++){
+                    let key = "semester_" + i;
+                    if(curriculum[key]){
+                        semesters.push(curriculum[key]);
+                    }
+                }
+            }   
+
+            res.status(200).send(semesters);
+        }
+    });
+});
+
+app.get('/api/plans', function(req, res){
+    Plan.find({}, function(err, data){
+        if(err){
+            res.status(400).send(err);
+        }
+        else{
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.get('/api/plan-names', function(req, res){
+    Plan.find({}, function(err, data){
+        if(err){
+            res.status(400).send(err);
+        }
+
+        else{
+            planNames = [];
+
+            for(plan in data){
+                planNames.push(data[plan]["planID"]);
+            }
+
+            res.status(200).send(planNames);
+        }
     });
 });
 
 app.delete('/api/delete-plan/:planID', function(req, res){
     Plan.deleteOne({"planID":req.params.planID}, function(err, data){
         if(err){
-            res.send(err);
+            res.status(400).send(err);
         }
 
-        res.send({data: "The plan was deleted"});
+        else{
+            res.status(200).send({data: "The plan was deleted"});
+        }
     });
 });
 
 app.get('/api/users', function(req, res){
     User.find({}, function(err, data){
         if(err){
-            res.send(err);
+            res.status(400).send(err);
         }
-
-        console.log(data);
-        res.send(data);
+        else{       
+            res.status(200).send(data);
+        }
     });
 });
 
 app.get('/api/delete-user/:studentID', function(req, res){
     User.deleteOne({"sid":req.params.studentID}, function(err, data){
         if(err){
-            res.send(err);
+            res.status(400).send(err);
         }
 
-        res.send({data: "The user was deleted"});
+        else{
+            res.status(200).send({data: "The user was deleted"});
+        }
     });
 });
 
-app.listen(port, () => console.log(`Backend listening on port ${port}!`))
+app.listen(port, () => console.log(`Scheduler API open on port ${port}!`))
 
 // https.createServer({
 //         key: fs.readFileSync('/var/secret/etc/ssl/forms-combined.cis.udel.edu.key'),
 //         cert: fs.readFileSync('/var/secret/etc/ssl/forms-combined.cis.udel.edu.pem')
 //     }, app)
-//     .listen(3000, function () {
+//     .listen(port, function () {
 //     console.log('API listening at: ' + serviceURL);
 // });
